fee714b95cff82f1000e067284b0e781c47c2ebd
ned <64433312+Neddys0404@users.noreply.github.com>
2026-03-04 13:13:17 +0800

    Defined encoders resolution
    Added encoder resolution definition and improved comments.

--- config.h
+++ config.h
@@ -7,6 +7,7 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
 
-// Encoder Pins
+// Encoder Pins and resolutions
 #define ENCODERS_PAD_A { B1, B3 }
 #define ENCODERS_PAD_B { B2, B4 }
+#define ENCODER_RESOLUTION 4
