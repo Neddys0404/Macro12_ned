574396e45c6457025077aee8a6a8b14d5798ed24
Neddys0404 <tepxcube@gmail.com>
2026-04-15 02:47:01 +0800

    attempt to fix rotation issue

--- config.h
+++ config.h
@@ -8,4 +8,6 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
 
 // Encoder Pins and resolutions
+#define ENCODERS_PAD_A { { GP3 }, { GP5 } }
+#define ENCODERS_PAD_B { { GP4 }, { GP6 } }
 #define ENCODER_RESOLUTION 4
