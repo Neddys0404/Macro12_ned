5875d718891281bd62a9d1ef7a63a2d31354268e
Neddys0404 <tepxcube@gmail.com>
2026-04-20 22:22:32 +0800

    update test remove encoder feature

--- config.h
+++ config.h
@@ -8,6 +8,6 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
 
 // Encoder Pins and resolutions
-#define ENCODER_A_PINS { GP3, GP5 }
-#define ENCODER_B_PINS { GP4, GP6 }
-#define ENCODER_RESOLUTION 4
+//#define ENCODER_A_PINS { GP3, GP5 }
+//#define ENCODER_B_PINS { GP4, GP6 }
+//#define ENCODER_RESOLUTION 4
